329e61a811f1ff94e5cc932dae8419a82a90fa01
kevin <kevinbabuvarkey@gmail.com>
2025-09-09 01:34:23 +0530

    sign up with validation complete

---
+++ erp_project/erp_app/validators.py
@@ -0,0 +1,53 @@
+from . models import CustomUser
+import imghdr
+import re
+
+def name_validator(name):
+    if not name:
+        return "Name cannot be empty"
+    else :
+        if len(name)<2:
+            return "Name must be greater than three digits"
+        else:
+            return None
+
+def image_validator(image):
+    if not image:
+        return "Image cannot be empty"
+    else:
+        if image.size > 5 * 1024 * 1024:  # 5MB limit
+            return "Image file size must be less than 5MB."
+
+        ext = image.name.split('.')[-1].lower()
+        if ext not in ['jpg', 'jpeg']:
+            return "Only JPEG images are allowed."
+
+    # Extra check to ensure it's a real JPEG file
+        if imghdr.what(image) not in ['jpeg']:
+            return "Invalid image format. Only JPEG is allowed."
+        return None
+
+def email_validator(email):
+    if not email:
+        return "Email cannot be empty"
+    elif CustomUser.objects.filter(email=email).exists():
+        return "This email already exists"
+    else:
+        if not re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-,]+$",email):
+            return "Invalid Email"
+        else:
+            return None
+
+def password_validator(password1 , password2):
+    if not password1 and not password2:
+        return "Password cannot be empty"
+    else:
+        if password1 != password2:
+            return "Passwords do not match"
+        if not re.match(r"^(?=.*[!@#$%^&*()_+{}:;\"'<>,.?/~`-]).{5,}$", password2):
+            return "Password must be atleast 5 long and must have one special characters"
+        else:
+            return None
+
+
+
--- erp_project/erp_app/views.py
+++ erp_project/erp_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import CustomUser
+from . validators import name_validator , image_validator , email_validator , password_validator
+from django.contrib.auth.hashers import check_password
 # Create your views here.
 
 
@@ -14,8 +16,24 @@ def signup(request):
         email = request.POST.get('email')
         password1 = request.POST.get('password1')
         password2 = request.POST.get('password2')
-        CustomUser.objects.create_user(full_name=name,email=email, image=image,
+        name_error = name_validator(name)
+        image_error = image_validator(image)
+        email_error = email_validator(email)
+        password_error = password_validator(password1,password2)
+        errors ={}
+        if name_error:
+            errors['name'] = name_error
+        if email_error:
+            errors['email'] = email_error
+        if image_error:
+            errors['image'] = image_error
+        if password_error:
+            errors['password'] = password_error
+        if errors:
+            return render(request, 'signup.html',{'error':errors})
+        else:
+            CustomUser.objects.create_user(full_name=name,email=email, image=image,
                                        password=password2)
-        return render(request, 'signup.html')
+            return redirect('home')
     else:
         return render(request, 'signup.html')

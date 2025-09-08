393c111d7d1583dc248ba11a1be7697836bd3620
kevin <kevinbabuvarkey@gmail.com>
2025-09-08 15:03:07 +0530

    sign up first stage complete

--- erp_project/erp_app/views.py
+++ erp_project/erp_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . models import CustomUser
 # Create your views here.
 
 
@@ -8,4 +8,14 @@ def home(request):           # login page / home page
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        name = request.POST.get('full_name')
+        image = request.FILES.get('image')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        CustomUser.objects.create_user(full_name=name,email=email, image=image,
+                                       password=password2)
+        return render(request, 'signup.html')
+    else:
+        return render(request, 'signup.html')
